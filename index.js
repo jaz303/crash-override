890c69ffde49689d14dfc7b1239369872e3a3066
Jason Frame <jason@onehackoranother.com>
2015-04-04 14:52:41 +0100

    tabs -> spaces

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var os = require('os');
 
 var impl = (function() {
-	switch (require('os').platform()) {
-		case 'darwin':
-			return require('./lib/darwin');
-		default:
-			throw new Error("unsupported platform");
-	}
+    switch (require('os').platform()) {
+        case 'darwin':
+            return require('./lib/darwin');
+        default:
+            throw new Error("unsupported platform");
+    }
 })();
 
 module.exports = impl;
