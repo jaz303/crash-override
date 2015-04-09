ee2f45d48b047a1fc19f97d5d5334bbb41164698
Jason Frame <jason@onehackoranother.com>
2015-04-09 17:24:37 +0100

    first stab at linux support

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var impl = (function() {
     switch (require('os').platform()) {
         case 'darwin':
             return require('./lib/darwin');
+        case 'linux':
+        	return require('./lib/linux');
         default:
             throw new Error("unsupported platform");
     }
